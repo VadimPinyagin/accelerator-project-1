d78c0d8e670452c6e6599f39a946da8074eb19be
Vadim <vadimbox85@gmail.com>
2025-11-19 13:13:15 +0300

    Исправляет размер слайдера и z-index меню

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,9 @@
 // https://swiperjs.com/get-started#installation
-import Swiper from "swiper";
-import {mainNavOpenClose} from "./moblie-nav.js";
-
-
+import {mainNavOpenClose} from './moblie-nav.js';
 import Swiper from 'swiper';
-// импортируем стили Swiper
 import 'swiper/css';
 
-// создаём экземпляр Swiper
-const swiper = new Swiper('.swiper', {
+new Swiper('.swiper', {
   // Опции Swiper
   loop: true, // бесконечный цикл
   pagination: {
