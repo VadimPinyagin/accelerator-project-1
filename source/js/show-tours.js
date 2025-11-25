7cd3d0cdbfe0a73ff0b33d3cddb92bbd6f48ccf9
Vadim <vadimbox85@gmail.com>
2025-11-25 17:04:57 +0300

    Дописана функция показа ближайших туров

--- source/js/show-tours.js
+++ source/js/show-tours.js
@@ -2,22 +2,32 @@ import '../sass/common/base.scss';
 import '../sass/common/vareables.scss';
 import {BODY, MOBILE_WIDTH} from './constants.js';
 
-const tours = BODY.querySelector(".tours");
-const btnShowTours = tours.querySelector(".tours__show-more");
-const toursCard = Array.from(tours.querySelectorAll(".tours-list"));
+const tours = BODY.querySelector('.tours');
+const btnShowTours = tours.querySelector('.tours__show-more');
+const toursCard = Array.from(tours.querySelectorAll('.tours-card'));
+
+const openTours = () => {
+  btnShowTours.addEventListener('click', () => {
+    toursCard.forEach((item) => item.classList.remove('hidden'));
+    btnShowTours.classList.add('hidden');
+  });
+};
 
 export const showTours = () => {
   window.addEventListener('resize', showTours);
   if (MOBILE_WIDTH.matches) {
-    btnShowTours.classList.remove("hidden");  // Если окно меньше 321px
+    btnShowTours.classList.remove('hidden');
   } else {
-    btnShowTours.classList.add("hidden"); // Удаляем класс, если больше
+    btnShowTours.classList.add('hidden');
   }
 
   toursCard.forEach((item,index) => {
-    item.classList.add('hidden')
-    if(index == 0) {
-      item.classList.remove('hidden')
+    if(index >= 1) {
+      item.classList.add('hidden');
+    } else if(index > 5) {
+      btnShowTours.classList.add('hidden');
     }
-  })
-}
+  });
+
+  openTours();
+};
