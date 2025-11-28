062322a5dfc5b812a025b1d724f24f267e0b7f62
VadimPinyagin <vadimbox85@gmail.com>
2025-11-28 06:46:24 +0300

    проверка медиазпросов для слайдературов

--- source/js/constants.js
+++ source/js/constants.js
@@ -1,2 +1,6 @@
+import '../sass/common/variables.scss'
 export const BODY = document.querySelector('.page__body');
-export const MOBILE_WIDTH = window.matchMedia("(max-width: 321px)");
+export const MOBILE_WIDTH = window.matchMedia("(max-width: 320px)");
+export const TABLET_WIDTH = window.matchMedia("(max-width: 768px)");
+export const DESkTOP_WIDTH = window.matchMedia("(max-width: 1440px)");
+
--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,17 @@
+import { MOBILE_WIDTH, TABLET_WIDTH, DESkTOP_WIDTH} from './constants.js';
 import {mainNavOpenClose} from './moblie-nav.js';
 import {initSwiperHero} from './swiper-hero.js';
 import { initSwiperTours } from './swiper-tours.js';
+
+
+  if(MOBILE_WIDTH.matches) {
+    console.log('Мобилка');
+  } else if (TABLET_WIDTH.matches) {
+    console.log('Планшетка');
+  } else if (DESkTOP_WIDTH.matches) {
+    console.log('Десктопка');
+  }
+
 
 mainNavOpenClose();
 initSwiperHero();
