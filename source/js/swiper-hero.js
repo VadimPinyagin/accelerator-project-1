af2de59b371ad741f495cfb7514b665cc607d93b
Vadim <vadimbox85@gmail.com>
2025-12-05 20:43:48 +0300

    Поправил слайдер hero

--- source/js/swiper-hero.js
+++ source/js/swiper-hero.js
@@ -1,11 +1,14 @@
 import Swiper from 'swiper';
 import {Pagination} from 'swiper/modules';
 
-export const initSwiperHero = new Swiper('.hero', {
-  modules: [Pagination],
-  direction: 'horizontal',
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-  }
-});
+export const initSwiperHero = () => {
+  new Swiper('.hero', {
+    modules: [Pagination],
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+};
